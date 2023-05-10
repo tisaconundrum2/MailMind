2b41fa54594633636ad29246ca567f7b4fa188b0
Nicholas Finch <ngf4@nau.edu>
2023-05-10 10:54:07 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,32 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+
+    nameInput.addEventListener('input', enableSubmitIfValid);
+    emailInput.addEventListener('input', enableSubmitIfValid);
+
+    enableSubmitIfValid(); // Call once to set initial button state
+});
+
+function isValidEmail(email) {
+    const regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    return regex.test(email);
+}
+
+function enableSubmitIfValid() {
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const submitButton = document.getElementById('submitButton');
+
+    if (nameInput.value && isValidEmail(emailInput.value)) {
+        submitButton.classList.remove('disabled');
+        submitButton.disabled = false;
+    } else {
+        submitButton.classList.add('disabled');
+        submitButton.disabled = true;
+    }
+}
